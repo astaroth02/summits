288368717e7d00132adb8e2251c61aa764591f30
Mohammad Mahdiani <it87.must@gmail.com>
2019-05-24 11:51:52 +0200

    add logs

--- nodejs/user-service/src/model/user.js
+++ nodejs/user-service/src/model/user.js
@@ -7,17 +7,19 @@ const store = new Store({ path: `${__dirname}/config.json` });
 module.exports = {
   save: user => {
     store.set(user.username, {...user, id: uuidv4()} );
-    console.log(`Added ${user.username}`);
+    console.log(`User-service : Added ${user.username}`);
   },
   get: username => {
-    console.log(username)
+    console.log(`User-service: get user with username: ${username}`);
     return store.get(username)
   },
   update: (username, user) => {
+    console.log(`User-service: update user with username: ${username}`);
     const getUser = store.get(username);
     store.set(username, {...getUser, ...user});
   },
   delete: (username) => {
+  console.log(`User-service: delete user with username: ${username}`);
    store.del(username); 
   }
 
