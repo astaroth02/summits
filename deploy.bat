92f2efb34e5cbeb82a1994a227476ded7b4ea79e
Marco Rosenthal <marco.rosenthal@virtual-identity.com>
2019-06-28 16:51:07 +0200

    - reworked folder handling - added syntax help

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,14 @@
 @SET version=v3
 @SET architecture=arm32v7
 
+if [%1]==[] (
+	goto End
+) else (
+	if not [%2]==[arm32v7] (
+		goto End
+	)
+) 
+
 @IF "%architecture%" == "arm32v7" (
     SET suffix=_%architecture%
 )
@@ -10,22 +18,21 @@
 @echo *******************************************
 @echo **  Update version for %servicename%     **
 @echo *******************************************
-@cd kubernetes
-node ..\setContainerTag.js summits-%servicename%%suffix%.yaml %version% %architecture%
+node setContainerTag.js kubernetes\summits-%servicename%%suffix%.yaml %version% %architecture%
 
 @SET servicename=general-service
 @echo
 @echo *******************************************
 @echo **  Update version for %servicename%     **
 @echo *******************************************
-node ..\setContainerTag.js summits-%servicename%%suffix%.yaml %version% %architecture%
+node setContainerTag.js kubernetes\summits-%servicename%%suffix%.yaml %version% %architecture%
 
 @SET servicename=user-service
 @echo
 @echo *******************************************
 @echo **  Update version for %servicename%     **
 @echo *******************************************
-node ..\setContainerTag.js summits-%servicename%%suffix%.yaml %version% %architecture%
+node setContainerTag.js kubernetes\summits-%servicename%%suffix%.yaml %version% %architecture%
 
 
 @echo
@@ -39,46 +46,46 @@ node ..\setContainerTag.js summits-%servicename%%suffix%.yaml %version% %archite
 @echo *******************************************
 @echo **  Docker build and push %servicename%  **
 @echo *******************************************
-@cd nodejs\%servicename%
-docker build . -t marcofenskevi/summits-%servicename%:%version%
-docker push marcofenskevi/summits-%servicename%:%version%
+@cd 
+docker build . -t nodejs\%servicename%\marcofenskevi/summits-%servicename%:%version%
+docker push nodejs\%servicename%\marcofenskevi/summits-%servicename%:%version%
 
-docker build . -t marcofenskevi/summits-%servicename%:arm32v7-%version%
-docker push marcofenskevi/summits-%servicename%:%architecture%-%version%
-@cd ..\..
+docker build . -t nodejs\%servicename%\marcofenskevi/summits-%servicename%:%architecture%-%version%
+docker push nodejs\%servicename%\marcofenskevi/summits-%servicename%:%architecture%-%version%
 
 @SET servicename=summit-service
 @echo
 @echo *******************************************
 @echo **  Docker build and push %servicename%  **
 @echo *******************************************
-@cd nodejs\%servicename%
-docker build . -t marcofenskevi/summits-%servicename%:%version%
-docker push marcofenskevi/summits-%servicename%:%version%
+docker build . -t nodejs\%servicename%\marcofenskevi/summits-%servicename%:%version%
+docker push nodejs\%servicename%\marcofenskevi/summits-%servicename%:%version%
 
-docker build . -t marcofenskevi/summits-%servicename%:%architecture%-%version%
-docker push marcofenskevi/summits-%servicename%:%architecture%-%version%
-@cd ..\..
+docker build . -t nodejs\%servicename%\marcofenskevi/summits-%servicename%:%architecture%-%version%
+docker push nodejs\%servicename%\marcofenskevi/summits-%servicename%:%architecture%-%version%
 
 @SET servicename=user-service
 @echo
 @echo *******************************************
 @echo **  Docker build and push %servicename%  **
 @echo *******************************************
-@cd nodejs\%servicename%
-docker build . -t marcofenskevi/summits-%servicename%:%version%
-docker push marcofenskevi/summits-%servicename%:%version%
+docker build . -t nodejs\%servicename%\marcofenskevi/summits-%servicename%:%version%
+docker push nodejs\%servicename%\marcofenskevi/summits-%servicename%:%version%
 
-docker build . -t marcofenskevi/summits-%servicename%:%architecture%-%version%
-docker push marcofenskevi/summits-%servicename%:%architecture%-%version%
-@cd ..\..
+docker build . -t nodejs\%servicename%\marcofenskevi/summits-%servicename%:%architecture%-%version%
+docker push nodejs\%servicename%\marcofenskevi/summits-%servicename%:%architecture%-%version%
 
 @echo
 @echo *******************************************
 @echo ** Deploy to Kubernetes Cluster          **
 @echo *******************************************
-@cd kubernetes
-kubectl apply -f summits-general-service%suffix%.yaml
-kubectl apply -f summits-user-service%suffix%.yaml
-kubectl apply -f summits-ascent-service%suffix%.yaml
-@cd ..\..
+kubectl apply -f kubernetes\summits-general-service%suffix%.yaml
+kubectl apply -f kubernetes\summits-user-service%suffix%.yaml
+kubectl apply -f kubernetes\summits-ascent-service%suffix%.yaml
+
+:End
+echo You need at least a version and optional an architecture (currently only 'arm32v7' is supported).
+echo Usage: deploy VERSION [ARCHITECTURE]"
+echo Examples:
+echo   deploy v3
+echo   deploy 23.12 arm32v7
